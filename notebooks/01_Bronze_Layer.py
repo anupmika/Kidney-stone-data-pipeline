9fe49185f0707c66cb6a92943fc1309048a3c8f6
Varnika <170813482+anupmika@users.noreply.github.com>
2026-05-29 09:46:59 +0530

    Update 01_Bronze_Layer.py

--- notebooks/01_Bronze_Layer.py
+++ notebooks/01_Bronze_Layer.py
@@ -1,1 +1,34 @@
-# Bronze Layer
+# Databricks notebook source
+dbutils.fs.ls("/Volumes/workspace/default/kidney_stone_volume/kidney-stone-dataset.csv")
+
+# COMMAND ----------
+
+df_bronze = spark.read\
+    .format("csv")\
+    .option("header", "true")\
+    .option("inferSchema", "true")\
+    .load("/Volumes/workspace/default/kidney_stone_volume/kidney-stone-dataset.csv")
+
+# COMMAND ----------
+
+df_bronze.show()
+
+
+# COMMAND ----------
+
+df_bronze.printSchema()
+
+# COMMAND ----------
+
+print(df_bronze.count())
+
+# COMMAND ----------
+
+df_bronze.write\
+    .format("delta")\
+    .mode("overwrite")\
+    .saveAsTable("bronze_kidney_stone")
+
+# COMMAND ----------
+
+spark.sql("select * from bronze_kidney_stone").show()
