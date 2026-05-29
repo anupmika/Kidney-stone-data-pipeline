19f543e4c1878c2de59f0e3c3b7fa5cf2382069c
Varnika <170813482+anupmika@users.noreply.github.com>
2026-05-29 09:54:03 +0530

    Implement analytics in Gold Layer for kidney stone data
    Added analytics and insights for kidney stone risk analysis in the gold layer, including average calcium, pH, osmolarity, and urea levels.

--- notebooks/03_Gold_Layer.py
+++ notebooks/03_Gold_Layer.py
@@ -1,1 +1,42 @@
-# Gold Layer
+PROJECT : Kidney Stone Risk Analysis
+LAYER : Gold Layer - Analytics & Insights
+AUTHOR : Anupmika
+DATE : 28 May 2026
+TOOL : PySpark + Databricks
+
+# Databricks notebook source
+df_gold = spark.sql("select * from silver_kidney_stone")
+
+# COMMAND ----------
+
+df_gold.groupBy("stone_present") \
+    .count() \
+    .orderBy("stone_present") \
+    .show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import avg, round
+
+df_gold.groupBy("stone_present") \
+    .agg(round(avg("calcium_level"), 2).alias("avg_calcium")) \
+    .show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import avg, round
+
+df_gold.groupBy("stone_present") \
+    .agg(
+        round(avg("urine_ph"), 2).alias("avg_ph"),
+        round(avg("osmolarity"), 2).alias("avg_osmolarity"),
+        round(avg("urea_level"), 2).alias("avg_urea")
+    ) \
+    .show()
+
+# COMMAND ----------
+
+df_gold.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("gold_kidney_stone")
